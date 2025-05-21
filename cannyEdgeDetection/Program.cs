80f728b3466f10a8d8d3ab843214bd1ca860d410
adriana-diaz <adrianadiaz3003@gmail.com>
2025-05-21 17:53:36 -0600

    proyecto de detección de bordes con canny
    Ejemplo en c# usando emgu cv (open cv para .net). convierte una imagen a escala de grises y aplica el algoritmo de canny para mostrar bordes detectados.

---
+++ cannyEdgeDetection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Emgu.CV; // Biblioteca principal de Emgu CV
+using Emgu.CV.Structure; // Permite usar tipos como Bgr (color) y Gray (escala de grises)
+
+namespace CannyEdgeDetection
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var image = new Image<Bgr, byte>("image3.jpg"); // Cargar la imagen (debe estar en la misma carpeta que el ejecutable)
+
+                var gray = image.Convert<Gray, byte>();// Convertir a escala de grises
+
+                var cannyEdges = gray.Canny(100, 200);// Aplicar detección de bordes con Canny
+
+                CvInvoke.Imshow("Bordes detectados (Canny)", cannyEdges);
+                CvInvoke.WaitKey(0); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message); // Captura errores
+            }
+        }
+    }
+}
